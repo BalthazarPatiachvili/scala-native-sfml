158a11ff6b573e084fa55e7af314745b60670d84
Vincent Lafeychine <vincent.lafeychine@proton.me>
2023-02-19 01:20:00 +0100

    feat(RectangleShape): Portage of RectangleShape

---
+++ src/main/scala/sfml/graphics/RectangleShape.scala
@@ -0,0 +1,29 @@
+package sfml
+package graphics
+
+import scalanative.unsafe.*
+import scalanative.unsigned.UnsignedRichLong
+
+import internal.graphics.RectangleShape.*
+
+import system.Vector2
+
+class RectangleShape private[sfml] (private val circleShape: ResourceBuffer[sfRectangleShape])
+    extends Shape(ResourceBuffer(circleShape.ptr.at1)):
+
+    private[sfml] inline def toNativeRectangleShape = circleShape.ptr
+
+    def this(size: Vector2[Float]) =
+        this(ResourceBuffer { (r: Ptr[sfRectangleShape]) =>
+            Zone { implicit z => ctor(r, size.toNativeVector2) }
+        })
+
+    override final def point(index: Long): Vector2[Float] =
+        Zone { implicit z =>
+            Vector2.toVector2Float(toNativeRectangleShape, index.toULong) { (data: Ptr[CStruct2[Ptr[sfRectangleShape], CSize]]) =>
+                sfRectangleShape_getPoint(data._1, data._2)
+            }
+        }
+
+    override final def pointCount: Long =
+        sfRectangleShape_getPointCount(toNativeRectangleShape).toLong
