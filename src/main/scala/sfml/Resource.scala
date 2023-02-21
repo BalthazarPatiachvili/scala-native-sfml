664fc5c2e9bb52eb36ec2d63b8b7fba9aa01339a
Vincent Lafeychine <vincent.lafeychine@proton.me>
2023-02-21 22:30:40 +0100

    fix(Warts): Add typing annotations to avoid Bottom type

--- src/main/scala/sfml/Resource.scala
+++ src/main/scala/sfml/Resource.scala
@@ -6,15 +6,14 @@ import scalanative.unsigned.UnsignedRichInt
 
 trait Resource extends AutoCloseable
 
-@SuppressWarnings(scala.Array("org.wartremover.warts.Nothing"))
 private[sfml] final class ResourceBuffer[T: Tag] private (buffer: Either[Ptr[T], ByteArray]):
 
     private[sfml] def this(ctor: Ptr[T] => Unit) =
-        this(Right(ByteArray.alloc(sizeof[T].toInt)))
+        this(Right[Ptr[T], ByteArray](ByteArray.alloc(sizeof[T].toInt)))
         ctor(ptr)
 
     private[sfml] def this(inner: Ptr[T]) =
-        this(Left(inner))
+        this(Left[Ptr[T], ByteArray](inner))
 
     private[sfml] inline def ptr: Ptr[T] =
         buffer match
